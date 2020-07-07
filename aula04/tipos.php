d52357443a3a85ba08b711c17dbe77ec2ae4ab78
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-07-06 22:52:50 -0300

    aula04

---
+++ aula04/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <?php
+        $idade = 29;
+        $nome = "Rossan";
+        echo "$nome tem $idade anos.<br>";
+        echo $nome. " tem ". $idade. " anos.";
+        ?>
+</div>
+</body>
+</html>
